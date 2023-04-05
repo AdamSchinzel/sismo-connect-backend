153f95a9d176ab58e354530a2be91750fc984f32
AdamSchinzel <schinzel.adam@gmail.com>
2023-04-05 06:56:48 +0200

    chore: post endpoint

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+global.atob = require("atob");
+
+import {
+  ZkConnect,
+  ZkConnectServerConfig,
+  AuthType,
+} from "@sismo-core/zk-connect-server";
+
+
+const zkConnectConfig: ZkConnectServerConfig = {
+  appId: "0xa4bd963b0de72eb2f23207d8ed9f6e88",
+};
+
+const zkConnect = ZkConnect(zkConnectConfig);
+
+
+const authRequest = {
+  authType: AuthType.ANON,
+};
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/verify", async (req, res) => {
+  const { groupId, zkConnectResponse } = req.body;
+
+  const claimRequest = {
+  groupId,
+};
+
+  try {
+    const { verifiedAuths } = await zkConnect.verify(zkConnectResponse, {
+      authRequest,
+      claimRequest,
+    });
+
+    if (verifiedAuths.length > 0) {
+        res.status(200)
+    }
+  } catch (e: any) {
+    res.status(400);
+  }
+});
+
+
+app.listen(process.env.PORT ?? 8080);
